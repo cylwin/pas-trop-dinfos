d9e95884ac3fc7ba3ddc0152fed27e710977d18a
Cyril BAREME <cyril.bareme@ensimag.grenoble-inp.fr>
2014-08-13 20:58:14 +0200

    lequipe.fr is bullshit haha

--- crawler/go.js
+++ crawler/go.js
@@ -11,7 +11,7 @@ feeds[0] = ["http://syndication.lesechos.fr/rss/rss_tech_medias.xml", "http://ww
 // , "http://www.leparisien.fr/high-tech/rss.xml", "http://www.lefigaro.fr/rss/figaro_hightech.xml", 
 feeds[1] = ["http://www.lexpress.fr/rss/politique.xml", "http://syndication.lesechos.fr/rss/rss_politique.xml", "http://www.lemonde.fr/emploi/rss_full.xml"];
 // "http://rss.liberation.fr/rss/13/", "http://rss.liberation.fr/rss/11/", 
-feeds[2] = ["http://www.lexpress.fr/rss/sport.xml", "http://www.lequipe.fr/rss/actu_rss.xml"];
+feeds[2] = ["http://www.lexpress.fr/rss/sport.xml"];
 // , "http://rss.liberation.fr/rss/14/"
 feeds[3] = ["http://www.lexpress.fr/rss/science-et-sante.xml"];
 feeds[4] = ["http://www.lepoint.fr/monde/rss.xml", "http://www.lemonde.fr/international/rss_full.xml"];
